fd969e4c16724213af48fa290dd7db8b20b79990
PritulaAlexey <pritula.alex91@mail.ru>
2023-05-23 19:45:06 +0700

    Create project

---
+++ ChaChaCha/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace ChaChaCha.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ ChaChaCha/ViewModels/ViewModelBase.cs
@@ -0,0 +1,8 @@
+﻿using ReactiveUI;
+
+namespace ChaChaCha.ViewModels
+{
+    public class ViewModelBase : ReactiveObject
+    {
+    }
+}
