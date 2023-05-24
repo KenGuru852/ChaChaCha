40035764dba94bbe7327d3d84bc777c885a14f32
PritulaAlexey <pritula.alex91@mail.ru>
2023-05-24 17:07:26 +0700

    All elements are aviable to draw from choosing him in menu and connector so

--- ChaChaCha/ViewModels/MainWindowViewModel.cs
+++ ChaChaCha/ViewModels/MainWindowViewModel.cs
@@ -2,17 +2,66 @@
 using ChaChaCha.Models;
 using ReactiveUI;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace ChaChaCha.ViewModels
 {
     public class MainWindowViewModel : ViewModelBase
     {
         private ObservableCollection<IElement> shapes;
-
+        //////////////////// ДЛЯ ДОБАВЛЕНИЯ ФИГУР НА ПОЛОТНО ////////////////////////////////
+        private int buttonpressed;
+        public int ButtonPressed
+        {
+            get => buttonpressed;
+            set => this.RaiseAndSetIfChanged(ref buttonpressed, value);
+        }
+        private int andButton;
+        public int AndButton
+        {
+            get => andButton;
+            set => this.RaiseAndSetIfChanged(ref andButton, value);
+        }
+        private int orButton;
+        public int OrButton
+        {
+            get => orButton;
+            set => this.RaiseAndSetIfChanged(ref orButton, value);
+        }
+        private int notButton;
+        public int NotButton
+        {
+            get => notButton;
+            set => this.RaiseAndSetIfChanged(ref notButton, value);
+        }
+        private int xorButton;
+        public int XorButton
+        {
+            get => xorButton;
+            set => this.RaiseAndSetIfChanged(ref xorButton, value);
+        }
+        private int smButton;
+        public int SMButton
+        {
+            get => smButton;
+            set => this.RaiseAndSetIfChanged(ref smButton, value);
+        }
+        private int inputButton;
+        public int InputButton
+        {
+            get => inputButton;
+            set => this.RaiseAndSetIfChanged(ref inputButton, value);
+        }
+        private int outputButton;
+        public int OutputButton
+        {
+            get => outputButton;
+            set => this.RaiseAndSetIfChanged(ref outputButton, value);
+        }
         public MainWindowViewModel()
         {
             Shapes = new ObservableCollection<IElement>();
-            Shapes.Add(new LogicElement
+/*            Shapes.Add(new LogicElement
             {
                 Height = 100,
                 FirstInput = true,
@@ -117,12 +166,19 @@ namespace ChaChaCha.ViewModels
                 SecondOutput = false,
                 ThirdOutput = false,
                 StartPoint = new Avalonia.Point(500, 300),
-            });
+            });*/
+            buttonpressed = 0;
         }
         public ObservableCollection<IElement> Shapes
         {
             get => shapes;
             set => this.RaiseAndSetIfChanged(ref shapes, value);
         }
+        
+        public void ElementToDraw(int number)
+        {
+            buttonpressed = number;
+        }
+        
     }
 }
--- ChaChaCha/Views/MainWindow.axaml.cs
+++ ChaChaCha/Views/MainWindow.axaml.cs
@@ -13,13 +13,161 @@ namespace ChaChaCha.Views
 {
     public partial class MainWindow : Window
     {
+        private Point pointPointerReleased;
         private Point pointPointerPressed;
         private Point pointerPositionIntoShape;
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        private void OnPointerReleased(object? sender, PointerReleasedEventArgs pointerReleasedEventArgs)
+        {
+            if (pointerReleasedEventArgs.Source is Control control)
+            {
+                if (control.DataContext is not LogicElement)
+                {
+                    pointPointerReleased = pointerReleasedEventArgs.GetPosition(
+                this.GetVisualDescendants().OfType<Canvas>().FirstOrDefault());
+                    if(this.DataContext is MainWindowViewModel viewModel)
+                    {
+                        if (viewModel.ButtonPressed == 1)
+                        {
+                            viewModel.Shapes.Add(new LogicElement
+                            {
+                                Height = 100,
+                                FirstInput = true,
+                                SecondInput = false,
+                                ThirdInput = true,
+                                FirstOutput = false,
+                                SecondOutput = true,
+                                ThirdOutput = false,
+                                Width = 100,
+                                Name = "And",
+                                RealName = "And",
+                                RecColor = "Black",
+                                StartPoint = pointPointerReleased,
+                            });
+                            viewModel.ButtonPressed = 0;
+                        }
+                        else if (viewModel.ButtonPressed == 2)
+                        {
+                            viewModel.Shapes.Add(new LogicElement
+                            {
+                                Height = 100,
+                                Width = 100,
+                                RealName = "Or",
+                                Name = "Or",
+                                RecColor = "Black",
+                                FirstInput = true,
+                                SecondInput = false,
+                                ThirdInput = true,
+                                FirstOutput = false,
+                                SecondOutput = true,
+                                ThirdOutput = false,
+                                StartPoint = pointPointerReleased,
+                            });
+                            viewModel.ButtonPressed = 0;
+                        }
+                        else if (viewModel.ButtonPressed == 3)
+                        {
+                            viewModel.Shapes.Add(new LogicElement
+                            {
+                                Height = 100,
+                                Width = 100,
+                                Name = "Not",
+                                RealName = "Not",
+                                RecColor = "Black",
+                                FirstInput = false,
+                                SecondInput = true,
+                                ThirdInput = false,
+                                FirstOutput = false,
+                                SecondOutput = true,
+                                ThirdOutput = false,
+                                StartPoint = pointPointerReleased,
+                            });
+                            viewModel.ButtonPressed = 0;
+                        }
+                        else if (viewModel.ButtonPressed == 4)
+                        {
+                            viewModel.Shapes.Add(new LogicElement
+                            {
+                                Height = 100,
+                                Width = 100,
+                                Name = "XOR",
+                                RealName = "XOR",
+                                RecColor = "Black",
+                                FirstInput = true,
+                                SecondInput = false,
+                                ThirdInput = true,
+                                FirstOutput = false,
+                                SecondOutput = true,
+                                ThirdOutput = false,
+                                StartPoint = pointPointerReleased,
+                            });
+                            viewModel.ButtonPressed = 0;
+                        }
+                        else if (viewModel.ButtonPressed == 5)
+                        {
+                            viewModel.Shapes.Add(new LogicElement
+                            {
+                                Height = 100,
+                                Width = 100,
+                                Name = "SM",
+                                RealName = "SM",
+                                RecColor = "Black",
+                                FirstInput = true,
+                                SecondInput = true,
+                                ThirdInput = true,
+                                FirstOutput = true,
+                                SecondOutput = false,
+                                ThirdOutput = true,
+                                StartPoint = pointPointerReleased,
+                            });
+                            viewModel.ButtonPressed = 0;
+                        }
+                        else if (viewModel.ButtonPressed == 6)
+                        {
+                            viewModel.Shapes.Add(new LogicElement
+                            {
+                                Height = 100,
+                                Width = 100,
+                                Name = "Input",
+                                RealName = "1",
+                                RecColor = "Green",
+                                FirstInput = false,
+                                SecondInput = false,
+                                ThirdInput = false,
+                                FirstOutput = false,
+                                SecondOutput = true,
+                                ThirdOutput = false,
+                                StartPoint = pointPointerReleased,
+                            });
+                            viewModel.ButtonPressed = 0;
+                        }
+                        else if (viewModel.ButtonPressed == 7)
+                        {
+                            viewModel.Shapes.Add(new LogicElement
+                            {
+                                Height = 100,
+                                Width = 100,
+                                Name = "Output",
+                                RealName = "1",
+                                RecColor = "Red",
+                                FirstInput = false,
+                                SecondInput = true,
+                                ThirdInput = false,
+                                FirstOutput = false,
+                                SecondOutput = false,
+                                ThirdOutput = false,
+                                StartPoint = pointPointerReleased,
+                            });
+                            viewModel.ButtonPressed = 0;
+                        }
+                    }
+                }
+            }
+            
+        }
         private void OnPointerPressed(object? sender, PointerPressedEventArgs pointerPressedEventArgs)
         {
             if (pointerPressedEventArgs.Source is Control control)
